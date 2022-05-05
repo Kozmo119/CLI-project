0d276023befebeb9e93a95a4d5b18266b4780afd
Mani <marhm005@gmail.com>
2022-05-05 09:47:00 +0100

    added Passenger Test

--- src/test/java/PassengerTest.java
+++ src/test/java/PassengerTest.java
@@ -1,2 +1,31 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class PassengerTest {
+    Passenger Mani;
+
+   @BeforeEach
+    public void setUp(){
+       Mani = new Passenger("Mani", 123,1234);
+   }
+
+   @Test
+    public void hasName(){
+       assertThat(Mani.getName()).isEqualTo("Mani");
+   }
+
+   @Test
+    public void hasPhoneNumber(){
+       assertThat(Mani.getPhoneNumber()).isEqualTo(123);
+   }
+
+   @Test
+    public void hasID(){
+   assertThat(Mani.getId()).isEqualTo(1234);
+   }
+
+
+
 }
